21da5b8ff05f2c4e98ab88dd34da00390d5eaccc
rg3915 <regis42santos@gmail.com>
2021-03-20 19:23:28 -0300

    Ajustes nos templates e logout.

--- authcustom/urls.py
+++ authcustom/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import login_view
 
 urlpatterns = [
     path('', login_view, name='login_view'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- authcustom/views.py
+++ authcustom/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
@@ -7,19 +8,17 @@ from authcustom.forms import *
 
 @csrf_protect
 def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
+    form = LoginForm(request.POST or None)
 
+    if request.method == 'POST':
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-
             user = authenticate(username=email, password=password)
-            if user is not None:
+            if user:
                 if user.is_active:
                     login(request, user)
-
-    else:
-        form = LoginForm()
+            msg_error = 'Usuário e senha não conferem.'
+            messages.error(request, msg_error)
 
     return render(request, 'authcustom/login.html', {'form': form})
